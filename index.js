b32ae16bc51510fde93b0aaa63af889d55e953b5
IgodoNdu <darkbluetechmail@gmail.com>
2022-12-27 00:32:43 +0100

    Middlewares
    Body parser for accepting user fed data, and setting the public/static view folder

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 //initialize express
 const app = express();
+
+//Enable body parser for accepting body data (req.body)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Set static folder
+app.use(express.static(path.join(__dirname, '../ai-image-gen-frontend')))
 
 app.use('/openai', require('./routes/openaiRoute'));
 
