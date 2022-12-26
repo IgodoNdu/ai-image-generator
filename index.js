2ed9c1bc8f20d2e5e30d0dc2bbd5e98c1d167921
IgodoNdu <darkbluetechmail@gmail.com>
2022-12-26 17:16:43 +0100

    Index setup
    Initialize express, with routing for image generation, and the appropriate port to listen on

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log(123);
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+//initialize express
+const app = express();
+
+app.use('/openai', require('./routes/openaiRoute'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
